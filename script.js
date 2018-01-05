c97b17b2c09063f791134738f69fab26675afdc4
Patrick <patrick.x.rivera@gmail.com>
2018-01-05 13:33:33 -0500

    Got rid of second event listener

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// when checkbox is clicked,
-  // save it as last checked;
-// if last checked has a value and shift key is held down while selecting a new checkbox,
-  // get index of last checked and check all boxes between it and the current checked box
-
 const checkboxes = Array.from(document.querySelectorAll('.inbox input[type=checkbox]'));
 
-function runEvent(e) {
-  let firstChecked = this;
+let lastChecked;
 
-  checkboxes.forEach(checkbox => checkbox.addEventListener('keydown', function(e) {
-    if (e.shiftKey) {
-      let start = getMin(firstChecked, this);
-      let end = getMax(firstChecked, this);
-      checkTargetBoxes(start, end)
-    }
-  }))
+function runEvent(e) {
+  if (e.shiftKey) {
+    getTargetBoxes(lastChecked, this);
+  }
+  lastChecked = this;
+}
+
+function getTargetBoxes(lastChecked, secondChecked) {
+  let start = getMin(lastChecked, secondChecked);
+  let end = getMax(lastChecked, secondChecked);
+  checkTargetBoxes(start, end)
 }
 
 function checkTargetBoxes(start, end) {
@@ -36,4 +34,4 @@ function getIndex(val) {
   return checkboxes.indexOf(val);
 }
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('change', runEvent))
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', runEvent))
